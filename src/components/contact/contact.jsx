3640e1924af350104293624a2ff39fb15d46a493
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-02-27 20:37:45 +0300

    Add files via upload

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () =>{
                     <div className='contact-name'>Невеста: +7 (906)-502-61-25</div>
                     <div className='contact-icon'>
                         <div className='contact-telegram'>
-                            <a href='https://t.me/@glirian_kesh' target="_blank">
+                            <a href='https://t.me/glirian_kesh' target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
   <path fill="#FFF" d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.894 8.221l-2.097 9.76c-.144.654-.524.817-1.062.508l-2.945-2.17-1.422 1.371a.785.785 0 0 1-.56.231l.213-3.003 5.454-4.925c.22-.196-.048-.276-.337-.101l-6.736 4.25-2.91-.913c-.643-.203-.65-.643.135-.954l11.566-4.458c.536-.232 1.006.128.832.941z"/>
 </svg>
